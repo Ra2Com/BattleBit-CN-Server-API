18055b7e5c02fe92380cd3fc4d73c6970a8dcb84
Maple Gao <esanisa@gmail.com>
2023-08-19 03:09:46 +0800

    bool

--- Component/IPlayerInfo.cs
+++ Component/IPlayerInfo.cs
@@ -20,9 +20,9 @@ namespace CommunityServerAPI.Component
         public ulong SteamID { get; }
         public int K { get; set; }
         public int D { get; set; }
-        public bool IsAdmin { get; set; };
-        public bool IsModerator { get; set;} ;
-        public bool IsVIP { get; set; };
+        public bool IsAdmin { get; set; }
+        public bool IsModerator { get; set;} 
+        public bool IsVIP { get; set; }
 
         public int rank { get; set; }
 
